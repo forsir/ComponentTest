e3a7bd58ede39fc3be81ed9e9bde6f524e80a629
forsir <forsi@gmail.com>
2022-02-26 12:18:11 +0100

    Add project files.

---
+++ ComponentTest1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ComponentTest1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
